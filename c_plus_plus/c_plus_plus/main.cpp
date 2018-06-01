aa19131b8cfdc33d9dbbb53aadc37943aec9de58
Mansi Goel <themansigoel@gmail.com>
2018-06-01 21:40:13 +0530

    initial commit for c++ project

---
+++ c_plus_plus/c_plus_plus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_plus_plus
+//
+//  Created by mansi goel on 01/06/18.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
